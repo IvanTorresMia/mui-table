28a44755aa5872589de78d833862d9b2234dbdb1
IvanTorresMia <ivantorresmia27@gmail.com>
2023-05-09 15:24:03 -0700

    initial app

---
+++ src/components/table.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+
+const Table: React.FunctionComponent = () => {
+
+    return (
+        <Box>
+
+        </Box>
+    )
+}
+
+export default Table;
